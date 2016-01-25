34f16b0776d1478cbdd2ff294bf6bf0b44f4f273
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-25 11:36:31 -0800

    Adds same files

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// JavaScript to be added
+var a = document.getElementById('button-yellow'); 
+		a.addEventListener('click', function() {
+			var response = prompt("Please provide your email", "name@email.com");
+		}, false);
+
