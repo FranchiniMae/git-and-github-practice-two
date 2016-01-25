84bf213dc0b702bd386f960ac2ddc3b4d4bafde4
Michael Norelli <mnorelli360@gmail.com>
2016-01-25 12:10:36 -0800

    Adds change to button text

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 // JavaScript to be added
+response = '';
 var a = document.getElementById('button-yellow'); 
-		a.addEventListener('click', function() {
-			var response = prompt("Please provide your email", "name@email.com");
-		}, false);
+a.addEventListener('click', function() {
+  response = prompt("Please provide your email", "name@email.com");
+  if (response) {
+  a.textContent="Thanks for your email!";
+}
+}, false);
+
+
+
+
 
